8e3c6abd0415bea5d672c743d57b810ceb75d886
Nithya Vijayan <niyavijayan@gmail.com>
2023-04-25 23:09:24 +0530

    eLearning website 25-04-2023

--- webApp/views.py
+++ webApp/views.py
@@ -64,27 +64,45 @@ def contactuss(request):
 def python_dev(request):
     return render(request,'python.html')
 
-# def react_dev(request):
-#     return render(request,'react.html')
+def react_dev(request):
+    return render(request,'react.html')
 
-# def rest_api(request):
-#     return render(request,'restapi.html')
+def rest_api(request):
+    return render(request,'restapi.html')
 
-# def digital_marketing(request):
-#     return render(request,'digital.html')
+def digital_marketing(request):
+    return render(request,'digital.html')
 
-# def frontend_dev(request):
-#     return render(request,'frontend.html')
+def frontend_dev(request):
+    return render(request,'frontend.html')
 
 def course_select(request,pk):
 
-    # if pk == 1:
-    return redirect('python_dev')
-    # elif pk == 2:
-    #     return render(request,'react.html')
-    # elif pk == 3:
-    #     return render(request,'frontend.html')
-    # elif pk == 4:
-    #     return render(request,'restapi.html')
-    # else:
-    #     return render(request,'digital.html')
+    # print(pk)
+    if pk == 1:
+        return redirect('python_dev')
+    elif pk == 2:
+        return render(request,'react.html')
+    elif pk == 3:
+        return render(request,'frontend.html')
+    elif pk == 4:
+        return render(request,'restapi.html')
+    else:
+        return render(request,'digital.html')
+
+
+def saveEnquiry(request):
+
+    if request.method == 'POST':
+        
+        name = request.POST.get('join_name')
+        email = request.POST.get('join_email')
+        phn = request.POST.get('join_phone')
+        course = request.POST.get('course')
+        message = request.POST.get('join_message')
+        print(name)
+        enquiry(name = name, email = email, phone = phn, course = course, message = message).save()
+
+    return redirect('index')
+
+
